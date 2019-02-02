efce7069b3e75abcd64d1851693956fa4853b135
Alina Heghedus <alinaheghedus@yahoo.com>
2019-02-02 00:01:24 -0500

    add click listener for infowindow, change color for marker and filter location list

--- js/app.js
+++ js/app.js
@@ -159,6 +159,16 @@ let ViewAppModel = () => {
 	    });
 	  };	
 
-	}
+	  // change clicked marker color
+	  self.makeMarkerIcon = function(markerColor) {
+	    var markerImage = new google.maps.MarkerImage('http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor + '|40|_|%E2%80%A2', new google.maps.Size(21, 34), new google.maps.Point(0, 0), new google.maps.Point(10, 34), new google.maps.Size(21, 34));
+	    return markerImage;
+	  };
+
+	  // default colour for marker
+	  var defaultIcon = makeMarkerIcon('d11aff');
+	  //when marker is clicked colour changes
+	  var highlightedIcon = makeMarkerIcon('6600ff');
+
 }
 
