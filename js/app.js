c6bd071ead5f6b982f576c65a0d81a9699007b80
Alina Heghedus <alinaheghedus@yahoo.com>
2019-02-03 20:29:14 -0500

    add comments

--- js/app.js
+++ js/app.js
@@ -1,106 +1,62 @@
 
-// global variables used
-let map;
-
-
-locations = [
+const locations = [
   {
     title: 'Downtown Toronto',
-    location: {
-      lat: 43.65323167517444,
-      lng: -79.38529600606677
-    },
+    location: {lat: 43.65323167517444, lng: -79.38529600606677},
     address: "Heart of the city",
     id: "5227bb01498e17bf485e6202",
     li: true
-  }, {
+  }, 
+  {
     title: 'Nathan Phillips Square',
-    location: {
-      lat: 43.65227047322295,
-      lng: -79.38351631164551
-    },
+    location: {lat: 43.65227047322295, lng: -79.38351631164551},
     address: "100 Queen St W",
     id: "4ad4c05ef964a520a6f620e3",
     li: true
-  }, {
+  }, 
+  {
     title: 'Eggspectation Bell Trinity Square',
-    location: {
-      lat: 43.65314383888587, 
-      lng: -79.38198016678167
-    },
+    location: {lat: 43.65314383888587, lng: -79.38198016678167},
     address: "483 Bay Street",
     id: "537773d1498e74a75bb75c1e",
     li: true
-  }, {
+  },
+  {
     title: 'Old City Hall',
-    location: {
-      lat: 43.652008800876125, 
-      lng: -79.3817442232328
-    },
+    location: {lat: 43.652008800876125, lng: -79.3817442232328},
     address: "60 Queen Street West",
     id: "4ad4c05ef964a5208ef620e3",
     li: true
-  }, {
+  }, 
+  {
     title: 'Japango',
-    location: {
-   	 lat: 43.65526771691681, 
-   	 lng: -79.38516506734886
-    },
+    location: {lat: 43.65526771691681, lng: -79.38516506734886},
     address: "122 Elizabeth St",
     id: "4ae7b27df964a52068ad21e3",
     li: true
-  }, {
+  }, 
+  {
     title: 'Textile Museum of Canada',
-    location: {
-      lat: 43.65439630500274, 
-      lng: -79.38650010906946
-    },
+    location: {lat: 43.65439630500274, lng: -79.38650010906946},
    	address: "55 Centre Avenue",
     id: "4ad4c05ef964a520e2f620e3",
     li: true
-  }, {
+  }, 
+  {
   	title: 'Indigo',
-    location: {
-      lat: 43.65351471121164,
-      lng: -79.38069591056922
-    },
+    location: {lat: 43.65351471121164, lng: -79.38069591056922},
     address: "220 Yonge St",
     id: "4b2a6eb8f964a52012a924e3",
     li: true
   }
 ];
 
-
-/* fetch the locations from foursquare api
-fetch('https://api.foursquare.com/v2/venues/explore?ll=43.653321,-79.384003&client_id=OGFWLIGUSRAL3E3YGMD1HTMLJP1SIRR4TIV1IHWGILDO2I1N&client_secret=YVZDERQD3UOAKRYN05SHLYODEIYXKSNXA2Q0RD0SNCX5QF4V&v=20180705')
-.then(response => response.json())
-.then(data => {
-	let locations = data.response.groups[0].items.map(item => {
-		return {
-			position: { lat: item.venue.location.lat, lng: item.venue.location.lng },
-			title: item.venue.name,
-			id: item.venue.id,
-			category: item.venue.categories[0].name,
-			address: item.venue.location.address,
-			state: item.venue.location.state,
-			postalCode: item.venue.location.postalCode
-		}
-	})	
-	console.log(locations);
-	(data) => {
-		
-	}
-})
-.catch(err => {
-	alert('Something went wrong. Please try again later.');
-})	
-*/
+let map;
 
 
-// initialise the map 
+// initialize the map 
 function initMap() {
-  //  a constructor to create a new map JS object. You can use the coordinates
-  //the coordinates of my city are given as center
+  // create the map using Toronto's centre coordinates
   map = new google.maps.Map(document.getElementById('map'), {
     center: {
       lat: 43.653321,
@@ -110,7 +66,7 @@ function initMap() {
   });
   largeInfoWindow = new google.maps.InfoWindow();
   bounds = new google.maps.LatLngBounds();
-  //call for our knockout view model
+
   ko.applyBindings(ViewAppModel());
 }
  
@@ -120,21 +76,21 @@ function mapError() {
 };
 
 
-// knockout viewmodel
+// the knockout viewmodel
 let ViewAppModel = () => {
 	selectedloc = ko.observable('');
 	let self = this;
 
-	//value ok knockout variable if foursquare doesn't load
+	// foursquare api error handling 
 	self.error = ko.observable('');
 
-	// markers array
+	// create markers array
 	self.markers = [];
 
-	// populates the infowindow when we click the marker
+	// populate the infowindow when we click a marker
 	self.populateInfoWindow = (marker, infowindow) => {
 
-		// fetch extra info from foursquare
+		// fetch extra info from foursquare api
 		$.ajax({
 	      url: "https://api.foursquare.com/v2/venues/" + marker.id + '?client_id=OGFWLIGUSRAL3E3YGMD1HTMLJP1SIRR4TIV1IHWGILDO2I1N&client_secret=YVZDERQD3UOAKRYN05SHLYODEIYXKSNXA2Q0RD0SNCX5QF4V&v=20180705',
 	      dataType: "json",
@@ -147,7 +103,7 @@ let ViewAppModel = () => {
 	        marker.description = '<div>' + "description=" + result.description + '</div>';
 
 
-	        // Check to see if the infowindow is not already opened on this marker.
+	        // make sure infowindow is not already open
 	        if (infowindow.marker != marker) {
 	          infowindow.marker = marker;
 	          infowindow.setContent('<div class="infowindowContent">' + marker.content + marker.place + marker.address + marker.description + '</div>');
@@ -163,30 +119,31 @@ let ViewAppModel = () => {
 	    });
 	  };	
 
-	  // change clicked marker color
+
+	  // change the clicked marker color
 	  self.makeMarkerIcon = function(markerColor) {
 	    var markerImage = new google.maps.MarkerImage('http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor + '|40|_|%E2%80%A2', new google.maps.Size(21, 34), new google.maps.Point(0, 0), new google.maps.Point(10, 34), new google.maps.Size(21, 34));
 	    return markerImage;
 	  };
 
+
 	  // default marker color
 	  var defaultColor = makeMarkerIcon('d11aff');
+
 
 	  // clicked marker color
 	  var clickedColor = makeMarkerIcon('6600ff');
 
 
-	  // create markers
+	  // create the markers for the provided locations
 	  for (i = 0; i < locations.length; i++) {
 
-	    // Get info from the locations array.
 	    let position = locations[i].location;
 	    let title = locations[i].title;
 	    let address = locations[i].address;
 	    let id = locations[i].id;
 	    let li = locations[i].li;
 
-	    // Create each marker
 	    let marker = new google.maps.Marker({
 	      map: map,
 	      position: position,
@@ -198,11 +155,14 @@ let ViewAppModel = () => {
 	      address: address
 	    });
 
+
 	    // Put the markers in an array
 	    self.markers.push(marker);
 
-	    // Extend map's boundary
+
+	    // Extend map's boundary to catch all markers
 	    bounds.extend(marker.position);
+
 
 	    // click event listener to open the infowindow
 	    // and change clicked marker's color
@@ -220,6 +180,7 @@ let ViewAppModel = () => {
 	    });	  
 	  }
 	  map.fitBounds(bounds);
+
 
 	  // filter the location list
 	  self.change = function(data, event) {
@@ -261,5 +222,4 @@ let ViewAppModel = () => {
 	    }
 	    largeInfoWindow.close();
 	  };
-}
-
+};
